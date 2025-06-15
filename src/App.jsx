8cbde67a13960895a75ef91294af2530bab5fda9
kdev307 <127479436+kdev307@users.noreply.github.com>
2025-06-15 17:42:02 +0530

    Added Navs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
@@ -9,6 +9,7 @@ import Login from "./pages/Login";
 import City from "./components/City";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import Form from "./components/Form";
 
 const BASE_URL = "http://localhost:8000";
 
@@ -40,7 +41,7 @@ function App() {
                 <Route path="pricing" element={<Pricing />} />
                 <Route path="login" element={<Login />} />
                 <Route path="app" element={<AppLayout />}>
-                    <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+                    <Route index element={<Navigate replace to="cities" />} />
                     <Route
                         path="cities"
                         element={<CityList cities={cities} isLoading={isLoading} />}
@@ -50,7 +51,7 @@ function App() {
                         path="countries"
                         element={<CountryList cities={cities} isLoading={isLoading} />}
                     />
-                    <Route path="form" element={<p>Form</p>} />
+                    <Route path="form" element={<Form />} />
                 </Route>
                 <Route path="*" element={<PageNotFound />} />
             </Routes>
