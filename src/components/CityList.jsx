09a8f040dbd7a0fa09fea23e5979c9f5ad5cba8e
kdev307 <127479436+kdev307@users.noreply.github.com>
2025-06-15 18:23:33 +0530

    Added Cities Context

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -2,12 +2,14 @@ import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
 import Message from "./Message";
-function CityList({ cities, isLoading }) {
+import { useCities } from "../contexts/CitiesContext";
+function CityList() {
+    const { cities, isLoading } = useCities();
     if (isLoading) return <Spinner />;
     if (!cities.length)
         return <Message message="Add your first city by clicking on a city on the map" />;
     return (
-        <ul className={styles.CityList}>
+        <ul className={styles.cityList}>
             {cities.map((city) => (
                 <CityItem city={city} key={city.id} />
             ))}
