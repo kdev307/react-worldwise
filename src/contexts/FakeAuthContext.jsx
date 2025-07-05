6689a66e34395441100478a21944ad74b24b3ca2
kdev307 <127479436+kdev307@users.noreply.github.com>
2025-07-05 13:36:56 +0530

    Implementing login and logout with programmatic navigation

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -1,19 +1,19 @@
 import { createContext, useContext, useReducer } from "react";
 
-const AuthContext = createContext()
+const AuthContext = createContext();
 const initialState = {
     user: null,
-    isAuthenticated: false
-}
+    isAuthenticated: false,
+};
 
-function reducer(state, action){
-    switch(action.type){
-        case 'login':
-            return {...state, user:action.payload, isAuthenticated:true}
-        case 'logout':
-            return {...state, user:null, isAuthenticated:false}
+function reducer(state, action) {
+    switch (action.type) {
+        case "login":
+            return { ...state, user: action.payload, isAuthenticated: true };
+        case "logout":
+            return { ...state, user: null, isAuthenticated: false };
         default:
-            throw new Error('Unknown action type')
+            throw new Error("Unknown action type");
     }
 }
 
@@ -24,30 +24,29 @@ const FAKE_USER = {
     avatar: "https://i.pravatar.cc/100?u=zz",
 };
 
+function AuthProvider({ children }) {
+    const [{ user, isAuthenticated }, dispatch] = useReducer(reducer, initialState);
 
-function AuthProvider({children}){
-
-    const [{user, isAuthenticated}, dispatch] = useReducer(reducer, initialState)
-
-    function login(email, password){
-        if(email === FAKE_USER.email & password === FAKE_USER.password)
-            dispatch({type:'login', payload:FAKE_USER})
+    function login(email, password) {
+        if ((email === FAKE_USER.email) & (password === FAKE_USER.password))
+            dispatch({ type: "login", payload: FAKE_USER });
     }
 
-    function logout(){
-        dispatch({type:'logout'})
+    function logout() {
+        dispatch({ type: "logout" });
     }
 
-    return(
-        <AuthContext.Provider value={{user, isAuthenticated, login, logout}}>{children}</AuthContext.Provider>
-    )
-
+    return (
+        <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
 }
 
-function useAuth(){
-    const context = useContext(AuthContext)
-    if(context === undefined)
-        throw new Error("AuthContext was used outside AuthProvider")
+function useAuth() {
+    const context = useContext(AuthContext);
+    if (context === undefined) throw new Error("AuthContext was used outside AuthProvider");
+    return context;
 }
 
-export default {useAuth, AuthProvider}
+export { useAuth, AuthProvider };
